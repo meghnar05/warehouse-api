656d0fbe428aa3627a28363af9f6757a54f73544
Meghna Reddy <meghna.reddy3005@gmail.com>
2025-11-27 23:20:53 -0500

    simple fastapi backend

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
-from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+orders = []
+
+class CustomerRequest(BaseModel):
+    customer_name: str
+    item: str
+    quantity: int
+    status: str = "requested"
+    order_id: int | None = None
+
+@app.post("/orders")
+def create_order(request: CustomerRequest): 
+    order = request.model_dump()
+    order["order_id"] = len(orders) + 1
+    orders.append(order)
+    return order
+
+@app.get("/orders")
+def get_orders():
+    return orders
+
+@app.get("/orders/{order_id}")
+def get_order(order_id: int):
+    for order in orders:
+        if order["order_id"] == order_id:
+            return order
+    raise HTTPException(status_code=404, detail="Invalid order ID")
+
+@app.patch("/orders/{order_id}")
+def update_order(order_id: int, status: str="processing"):
+    order = get_order(order_id)
+    order["status"] = status
+    return {"updated_order": order}
+
+@app.delete("/orders/{order_id}")
+def delete_order(order_id: int):
+    for order in orders:
+        if order["order_id"] == order_id:
+            orders.remove(order)
+            return {"deleted": order}
+    raise HTTPException(status_code=404, detail="Invalid order ID")
---
+++ old_main.py
@@ -0,0 +1,144 @@
+from enum import Enum
+
+from typing import Any
+
+from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"}
+]
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+
+@app.get("/")
+async def root():
+    return {"message": "hello"}
+
+@app.get("/ping")
+async def ping():
+    return {"ok": True}
+
+@app.get("/hello/{name}")
+async def greeting(name: str):
+    return {"message": f"hello {name}"}
+
+@app.get("/square")
+async def square(q: int):
+    return {"result": q*q}
+
+@app.post("/echo")
+def echo(data: dict):
+    return {"you_sent": data}
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int):
+#     return {"item id": item_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Depp Learning FTW!"}
+    
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+# ================================================================================================
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: list[str] = []
+
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item) -> Any:
+    return item
+
+@app.get("/items/", response_model=list[Item])
+async def read_items() -> Any:
+    return [
+        {"name": "Portal Gun", "price": 42.0},
+        {"name": "Plumbus", "price": 32.0},
+    ]
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
+
+# class UserIn(BaseModel):
+#     username: str
+#     password: str
+#     email: EmailStr
+#     full_name: str | None = None
+
+
+# class UserOut(BaseModel):
+#     username: str
+#     email: EmailStr
+#     full_name: str | None = None
+
+
+# @app.post("/user/", response_model=UserOut)
+# async def create_user(user: UserIn) -> Any:
+#     return user
+
+class BaseUser(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+class UserIn(BaseUser):
+    password: str
+
+
+@app.post("/user/")
+async def create_user(user: UserIn) -> BaseUser:
+    return user
